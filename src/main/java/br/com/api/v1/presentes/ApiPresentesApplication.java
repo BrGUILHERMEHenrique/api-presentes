80b4a396ac4dfafce4ea65b239a383f454be167d
System administrator <guilhermehenriquersouza17@gmail.com>
2023-10-02 20:38:31 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/api/v1/presentes/ApiPresentesApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.v1.presentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPresentesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPresentesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/v1/presentes/ApiPresentesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.v1.presentes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPresentesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
